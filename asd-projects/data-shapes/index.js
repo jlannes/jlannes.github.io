99ed2c5a13e62691625af48ffe12237eec26c213
jlannes <lannes185@stcharles.k12.la.us>
2024-01-26 15:51:52 +0000

    saving data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -68,8 +68,11 @@ $(document).ready(function () {
   }
 
   // TODO 5-a: add a function that handles the bad display type
-  
-
+  function handleBad(data, repeat) {
+    repeat += 1
+    setBackgroundWithMixed(data, repeat)
+    animationDetails.displayType = 3
+  }
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
   /////////////////////////////////////////////////
@@ -87,7 +90,9 @@ $(document).ready(function () {
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+    var currentShape = dataShapes[currentIndex]
+    var repeat = currentShape.repeat
+    handleBad(currentShape, repeat)
   }
 
   /////////////////////////////////////////////////
