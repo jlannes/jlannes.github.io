6d829e7c814e1f4fd8b073a2c855761dd4daad16
jlannes <lannes185@stcharles.k12.la.us>
2024-04-19 16:00:17 +0000

    some changes on my game

--- asd-projects/my-game/index.js
+++ asd-projects/my-game/index.js
@@ -1,17 +1,33 @@
 //Initialization
 var points = 0
 var pointsPerSecond = 1
+$("#addPoint").on("click", addPoints)
 $("#upgrade1").on("click", upgrade1Click)
-setInterval(update, 1000/60)
+
 setInterval(updatePoints, 1000)
 
 //Main functions
-function update(){
-    $("#points").text("points: " + points)
-}
 function updatePoints() {
     points += pointsPerSecond
+    updatePointCounter(points)
+    $("#pps").text("pps: " + pointsPerSecond)
+    pointsPerSecond += pointsPerSecond
 }
+
+function addPoints() {
+    points += 1
+    updatePointCounter(points)
+}
+
 function upgrade1Click() {
     pointsPerSecond += 1
+    $("#pps").text("pps: " + pointsPerSecond)
+}
+function updatePointCounter(points) {
+    toString(points)
+    console.log(points)
+    $("#points").text("points: " + points)
+    // if(points.length > 9) {
+
+    // }
 }
