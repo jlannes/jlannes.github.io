aab766a0d78d15a44fd8e6e3c1766ba3751fb51c
jlannes <lannes185@stcharles.k12.la.us>
2024-05-06 13:24:57 +0000

    pushing pong

--- asd-projects/my-game/index.js
+++ asd-projects/my-game/index.js
@@ -1,34 +1,61 @@
 //Initialization
+//important stuff
 var points = 0
-var pps = 0
 var tickrate = 0
-$("#addPoint").on("click", addPoints)
-$("#upgrade1").on("click", upgrade1Click)
 
-setInterval(updatePoints, tickrate)
+//pps
+var ppsBase = 0
+var ppsMult = 1
+var ppsTotal = 0
+
+//upgrades
+var upg1Cost = 10
+var upg2Cost = 50
+
+setInterval(update, tickrate)
 
 //Main functions
-function updatePoints() {
+function update() {
     updatePointCounter(points)
     updatePointAmount()
+    ppsTotalChange()
 }
 
 function updatePointAmount() {
-    points += pps/150
-    $("#pps").text("pps: " + pps)
+    points += (ppsBase*ppsMult)/150
+    $("#pps").text("pps: " + ppsTotal)
 }
 
 function addPoints() {
-    points += 1 + pps/2
+    points += 1 + ppsTotal/2
     updatePointCounter(points)
 }
 
 function upgrade1Click() {
-    pps += 1
-    $("#pps").text("pps: " + pps)
+    if(points >= upg1Cost) {
+        points -= upg1Cost
+        upg1Cost += 10
+        ppsBase += 1
+        $("#pps").text("pps: " + ppsTotal)
+        $("#upgrade1").text("Add 1 to pps base(cost: " + upg1Cost + ")")
+    }
 }
 
-// Factory functions
+function upgrade2Click() {
+    if(points >= upg2Cost) {
+        points -= upg2Cost
+        upg2Cost *= 2
+        ppsMult += 1
+        $("#pps").text("pps: " + ppsTotal)
+        $("#upgrade2").text("Add 1 to pps mult(cost: " + upg2Cost + ")")
+    }
+}
+
+function ppsTotalChange() {
+    ppsTotal = ppsBase*ppsMult
+}
+
+// helper functions
 function updatePointCounter() {
     pointTemp = Math.floor(points)
     if(pointTemp >= 1000000) {
